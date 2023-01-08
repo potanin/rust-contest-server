09bc034cf93caf98538b71df82543475ac2b2117
Alex Potanin <alex.potanin@anu.edu.au>
2023-01-08 14:17:02 +1300

    Tidy up.

--- contest_server/src/main.rs
+++ contest_server/src/main.rs
@@ -4,7 +4,7 @@ fn main() {
     contest_server::submission2::run();
     contest_server::submission3::run();
     
-    // submission4::run(); // This one is interactive so better not run every time.
+    // submission4::run(); // Submission 4 uses "std::io" - how can we stop it???
 
     println!("And finally: {}.", contest_server::submission1::fib(42));
 }
